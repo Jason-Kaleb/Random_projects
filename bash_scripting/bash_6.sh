d52ec617f2ed4948b195cfe74595ac94f6d64887
SiN-Kali <kyleskaleb56@gmail.com>
2023-02-05 22:40:04 +0200

    Edited Bash_6.sh

--- bash_scripting/bash_6.sh
+++ bash_scripting/bash_6.sh
@@ -1,13 +1,19 @@
 #!/bin/bash 
 
+echo "This is a Lap Counter. For Each Lap You Complete, Press Enter."
+
+sleep 2 
+
+echo "Number of Laps Required: 10"
+
 x=1
 
 while [[ $x -le 10 ]]
 do 
-  read -p "Push-Up Number: $x"
+  read -p "Lap Number: $x"
   (( x ++ ))
 done
 sleep 1 
 
-echo "Congrats,You Have Completed Your Push-Ups!!"
+echo "Congrats,You Have Completed Your Round Of laps!!"
 
