67f333d672276226b867caecaf437e3687c8dbb9
SiN-Kali <kyleskaleb56@gmail.com>
2023-06-02 15:12:30 +0200

    My first C++ Program

---
+++ c++/myfirstprogram.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/*
+ * main - entry point
+ * @description - prints Hello World!
+ * Return: 0 = always success
+ */
+
+ int main(void)
+ {
+         std::cout << "Hello World!\n";
+
+         return (0);
+ }
